075311688338d058e724528029f824f154de046e
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2021-11-21 16:05:28 +0100

    add dotenv module to use .env files

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
 const path = require('path');
 var app =express();
 app.use(session({
