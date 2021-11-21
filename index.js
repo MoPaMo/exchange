d7f8f2d3cad429b0187620ac28686f025f8071e8
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2021-11-21 16:07:13 +0100

    correctly set up dotenv

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
+const dotenv = require('dotenv').config();
 const path = require('path');
 var app =express();
 app.use(session({
