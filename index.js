11335ca6467974876e564ae3ced3cb0c2dadaeec
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2021-11-21 16:17:15 +0100

    send item.html

--- index.js
+++ index.js
@@ -4,19 +4,40 @@ const bodyParser = require('body-parser');
 const mustache = require("mustache");
 const dotenv = require('dotenv').config();
 const path = require('path');
-var app =express();
+const fs = require("fs");
+
+var app = express();
 app.use(session({
-	secret: process.env.session_pwd,
-	resave: true,
-	saveUninitialized: true
+  secret: process.env.session_pwd,
+  resave: true,
+  saveUninitialized: true
 }));
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')))
+
+const renderFile = function(path, data, cb) {
+  fs.readFile(path, (err, buff) => {
+    // if any error
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      //console.log(buff);
+      let text = buff.toString();
+      //console.log(text);
+      cb(mustache.render(text, data));
+    }
+  });
+};
+
 app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/views/index.html`);
 });
 app.get("/item/:id", (req, res) => {
   res.sendFile(`${__dirname}/views/item.html`);
+
 });
 app.listen(3000);
